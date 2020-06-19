9e783c6dae52bf7f1479b8eb57bd718fe7e44f7b
Rajan Mudambi <11376379+rmudambi@users.noreply.github.com>
2020-06-18 18:58:40 -0700

    update README and add improvements from archival

--- {{cookiecutter.package_name}}/src/{{cookiecutter.package_name}}/paths.py
+++ {{cookiecutter.package_name}}/src/{{cookiecutter.package_name}}/paths.py
@@ -2,6 +2,8 @@ from pathlib import Path
 
 import {{cookiecutter.package_name}}.globals as project_globals
 
+BASE_DIR = Path({{cookiecutter.package_name}}.__file__).resolve().parent
+
 ARTIFACT_ROOT = Path(f"/share/costeffectiveness/artifacts/{project_globals.PROJECT_NAME}/")
-MODEL_SPEC_DIR = (Path(__file__).parent / 'model_specifications').resolve()
+MODEL_SPEC_DIR = BASE_DIR / 'model_specifications'
 RESULTS_ROOT = Path(f'/share/costeffectiveness/results/{project_globals.PROJECT_NAME}/')
