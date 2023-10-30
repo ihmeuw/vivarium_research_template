33f30047837220372f4f157c65d0abccb5b95334
Steve Bachmeier <sbachmei@uw.edu>
2023-10-30 10:57:35 -0700

    delete unused paths

--- {{cookiecutter.package_name}}/src/{{cookiecutter.package_name}}/constants/paths.py
+++ {{cookiecutter.package_name}}/src/{{cookiecutter.package_name}}/constants/paths.py
@@ -6,5 +6,4 @@ from {{cookiecutter.package_name}}.constants import metadata
 BASE_DIR = Path({{cookiecutter.package_name}}.__file__).resolve().parent
 
 ARTIFACT_ROOT = Path(f"/mnt/team/simulation_science/pub/models/{metadata.PROJECT_NAME}/artifacts/")
-MODEL_SPEC_DIR = BASE_DIR / "model_specifications"
-RESULTS_ROOT = Path(f"/mnt/team/simulation_science/pub/models/{metadata.PROJECT_NAME}/results/")
+
