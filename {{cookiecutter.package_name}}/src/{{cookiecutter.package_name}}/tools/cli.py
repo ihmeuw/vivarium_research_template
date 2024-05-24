3d8c4201700caf465378f0a247ba90a6551925d2
Hussain Jafari <hussainsjafari@gmail.com>
2024-05-24 12:00:53 -0700

    lint

--- {{cookiecutter.package_name}}/src/{{cookiecutter.package_name}}/tools/cli.py
+++ {{cookiecutter.package_name}}/src/{{cookiecutter.package_name}}/tools/cli.py
@@ -11,52 +11,78 @@ from {{cookiecutter.package_name}}.tools import (build_artifacts,
 
 
 @click.command()
-@click.option('-l', '--location',
-              default='all',
-              show_default=True,
-              type=click.Choice(metadata.LOCATIONS + ['all']),
-              help=('Location for which to make an artifact. Note: prefer building archives on the cluster.\n'
-                    'If you specify location "all" you must be on a cluster node.'))
-@click.option('--years',
-              default=None,
-              help=('Years for which to make an artifact. Can be a single year or "all". If not specified, make for most recent year.'))
-@click.option('-o', '--output-dir',
-              default=str(paths.ARTIFACT_ROOT),
-              show_default=True,
-              type=click.Path(),
-              help='Specify an output directory. Directory must exist.')
-@click.option('-a', '--append',
-              is_flag=True,
-              help='Append to the artifact instead of overwriting.')
-@click.option('-r', '--replace-keys',
-              multiple=True,
-              help='Specify keys to overwrite')
-@click.option('-v', 'verbose',
-              count=True,
-              help='Configure logging verbosity.')
-@click.option('--pdb', 'with_debugger',
-              is_flag=True,
-              help='Drop into python debugger if an error occurs.')
-def make_artifacts(location: str, years: Optional[str], output_dir: str, append: bool, replace_keys: Tuple[str, ...],
-                   verbose: int, with_debugger: bool) -> None:
+@click.option(
+    "-l",
+    "--location",
+    default="all",
+    show_default=True,
+    type=click.Choice(metadata.LOCATIONS + ["all"]),
+    help=(
+        "Location for which to make an artifact. Note: prefer building archives on the cluster.\n"
+        'If you specify location "all" you must be on a cluster node.'
+    ),
+)
+@click.option(
+    "--years",
+    default=None,
+    help=(
+        "Years for which to make an artifact. Can be a single year or 'all'. \n"
+        "If not specified, make for most recent year."
+    ),
+)
+@click.option(
+    "-o",
+    "--output-dir",
+    default=str(paths.ARTIFACT_ROOT),
+    show_default=True,
+    type=click.Path(),
+    help="Specify an output directory. Directory must exist.",
+)
+@click.option(
+    "-a", "--append", is_flag=True, help="Append to the artifact instead of overwriting."
+)
+@click.option("-r", "--replace-keys", multiple=True, help="Specify keys to overwrite")
+@click.option("-v", "verbose", count=True, help="Configure logging verbosity.")
+@click.option(
+    "--pdb",
+    "with_debugger",
+    is_flag=True,
+    help="Drop into python debugger if an error occurs.",
+)
+def make_artifacts(
+    location: str,
+    years: Optional[str],
+    output_dir: str,
+    append: bool,
+    replace_keys: Tuple[str, ...],
+    verbose: int,
+    with_debugger: bool,
+) -> None:
     configure_logging_to_terminal(verbose)
     main = handle_exceptions(build_artifacts, logger, with_debugger=with_debugger)
     main(location, years, output_dir, append or replace_keys, replace_keys, verbose)
 
 
 @click.command()
-@click.argument('output_file', type=click.Path(exists=True))
-@click.option('-v', 'verbose',
-              count=True,
-              help='Configure logging verbosity.')
-@click.option('--pdb', 'with_debugger',
-              is_flag=True,
-              help='Drop into python debugger if an error occurs.')
-@click.option('-s', '--single', 'single_run',
-              default=False,
-              is_flag=True,
-              help='Results are from a single, non-parallel run.')
-def make_results(output_file: str, verbose: int, with_debugger: bool, single_run: bool) -> None:
+@click.argument("output_file", type=click.Path(exists=True))
+@click.option("-v", "verbose", count=True, help="Configure logging verbosity.")
+@click.option(
+    "--pdb",
+    "with_debugger",
+    is_flag=True,
+    help="Drop into python debugger if an error occurs.",
+)
+@click.option(
+    "-s",
+    "--single",
+    "single_run",
+    default=False,
+    is_flag=True,
+    help="Results are from a single, non-parallel run.",
+)
+def make_results(
+    output_file: str, verbose: int, with_debugger: bool, single_run: bool
+) -> None:
     configure_logging_to_terminal(verbose)
     main = handle_exceptions(build_results, logger, with_debugger=with_debugger)
     main(output_file, single_run)
