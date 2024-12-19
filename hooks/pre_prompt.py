438d9c9540b878460f92938cc42eb9af68c7a779
Abraham Flaxman <abie@uw.edu>
2024-12-18 17:53:09 -0800

    maybe this filename will work

--- hooks/pre_prompt.py
+++ hooks/pre_prompt.py
@@ -1,7 +1,7 @@
 import os
 import json
 import requests
-print("DEBUG: pre_gen_project.py is running...")
+print("DEBUG: pre_prompt.py is running...")
 
 def get_latest_version(package_name):
     """Fetch the latest version of a package from PyPI."""
